dc13e85453de27f827ca9abe47ca89c7cffb18ae
Simone Pozzobon <simone.pozzobon@zebra.com>
2025-04-29 16:35:18 +0200

    Middleware updated (Token removed) + Full Velocity profile added to the repo

--- velocity-script-lauch-zebra-dimensioning.js
+++ velocity-script-lauch-zebra-dimensioning.js
@@ -1,42 +1,36 @@
 
-Logger.debug("ZEBRA Dimensioning - Launch");
+  Logger.debug("ZEBRA Dimensioning - Launch");
 
-//Create the values that will be passed to the action.
-var actionName = "com.spozebra.zebradimensioningactivityforresult.MainActivity";
+  //Create the values that will be passed to the action.
+  var actionName = "com.sample.mdintegrationapp.GET_DIMENSION";
 
-//Use either action, class and package, or both. Define extras with a name, value, and type.
-//var intent = { action: actionName, extras: [ ] };
-var intent = {
-	action: 'android.intent.action.MAIN',
-	package: "com.spozebra.zebradimensioningactivityforresult",
-	class: "com.spozebra.zebradimensioningactivityforresult.MainActivity",
-};
-//Launch the action and mark the input as handled.
-Action.launchForResult(intent, function (intent) {
-	Logger.debug("Intent = " + JSON.stringify(intent));
-	if (intent.result == -1) {
-		var length = 0;
-		var width = 0;
-		var height = 0;
+  //Use either action, class and package, or both. Define extras with a name, value, and type.
+  //var intent = { action: actionName, extras: [ ] };
+  var intent = {  
+	action: actionName,
+    	package: "com.sample.mdintegrationapp",
+    	class: "com.sample.mdintegrationapp.MainActivity",
+ };
+  //Launch the action and mark the input as handled.
+  Action.launchForResult(intent, function(intent) {
+    Logger.debug("Intent = " + JSON.stringify(intent));
+    if (intent.result == -1) {
+	var length = 0;
+      	var width = 0;
+	var height = 0;
 
-		var extras = intent.extras
-		for (pos in extras) {
-			Logger.debug("Extra = " + JSON.stringify(extras[pos]));
-			if (extras[pos].name == "length") {
-				length = extras[pos].value;
-			}
-			else if (extras[pos].name == "width") {
-				width = extras[pos].value;
-			}
-			else if (extras[pos].name == "height") {
-				height = extras[pos].value;
-			}
-		}
-		Logger.debug("ZEBRA Dimensioning - Launch - Script Success");
-		var volume = length * width * height;
-		Device.sendKeys(volume);
-	}
-});
+	var dimObj =  JSON.parse(intent.extras[0].value);
+       Logger.debug("Extra = " + JSON.stringify(dimObj));
 
-Logger.debug("ZEBRA Dimensioning - Launch Ended");
+       length = parseFloat(dimObj.length);
+       width = parseFloat(dimObj.width);
+       height = parseFloat(dimObj.height);
 
+	View.toast(length, true);
+      Logger.debug("ZEBRA Dimensioning - Launch - Script Success");
+	var volume = length * width * height;
+ 	Device.sendKeys(volume);
+    }
+  });
+
+  Logger.debug("ZEBRA Dimensioning - Launch Ended");
